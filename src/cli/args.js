e62d41d588ad3fe6be56ccade104e5f21c116dcb
Asta <asta84@gmail.com>
2025-10-26 20:40:14 +0200

    feat: add cli solutions

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,5 +1,14 @@
 const parseArgs = () => {
-  // Write your code here
-};
+  const res = process.argv.slice(2);
+  // considering that all pairs have both keys and values
+  const length = res.length / 2
+  const keys = res.filter((arg, i) => i % 2 === 0 && arg.startsWith('--'))
+  const cleanKeys = keys.map(key => key.replace(/^--/, ''))
+  const values = res.filter((arg, i) => i % 2 === 1 && !arg.startsWith('--') )
+  for (let i = 0; i < length; i++)
+    if (cleanKeys !== undefined && values !== undefined)
+    console.log(`${cleanKeys[i]} is ${values[i]}`)
+
+}
 
 parseArgs();
